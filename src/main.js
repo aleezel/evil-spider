e13335832f44befd8bd430f46298e19ee3cc47b1
Alejandra Piedra <89430034+aleezel@users.noreply.github.com>
2025-04-02 15:44:20 -0600

    fix canva id

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 4;
 
-const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('#canvas'), antialias: true });
+const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('#effects'), antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 
