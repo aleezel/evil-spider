76f692ed366811d83ac9deb1e551574afbe31b7b
Alejandra Piedra <89430034+aleezel@users.noreply.github.com>
2025-04-02 16:01:36 -0600

    quitar luz

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 4;
 
-const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('#effects'), antialias: true });
+const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('#effects'), antialias: true, alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 
@@ -20,9 +20,9 @@ renderer.setPixelRatio(window.devicePixelRatio);
 //scene.add(cube);
 
 // Luz
-const light = new THREE.DirectionalLight(0xffffff, 1);
-light.position.set(5, 5, 5);
-scene.add(light);
+//const light = new THREE.DirectionalLight(0xffffff, 1);
+//light.position.set(5, 5, 5);
+//scene.add(light);
 
 // EffectComposer
 const composer = new EffectComposer(renderer);
