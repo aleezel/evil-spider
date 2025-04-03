c76aee34374c54d41aff246be9c7c66e74cf7a78
aleezel <alejandra.piedra02@gmail.com>
2025-04-02 22:26:57 -0600

    se comenta ojo de pez para ver si se ve mas el filmapass

--- src/main.js
+++ src/main.js
@@ -18,12 +18,12 @@ const composer = new EffectComposer(renderer);
 const renderPass = new RenderPass(scene, camera);
 composer.addPass(renderPass);
 
-const filmPass = new FilmPass(0.35, 0.5, 300, 0,)
+const filmPass = new FilmPass(0.9, 0.5, 300, 0,)
 composer.addPass(filmPass)
 
-const fishEyePass = new ShaderPass(FishEyeShader);
-fishEyePass.uniforms.strength.value = 0.35; // puedes ajustarlo
-composer.addPass(fishEyePass);
+// const fishEyePass = new ShaderPass(FishEyeShader);
+// fishEyePass.uniforms.strength.value = 0.35; // puedes ajustarlo
+// composer.addPass(fishEyePass);
 
 // Resize handler
 window.addEventListener('resize', () => {
