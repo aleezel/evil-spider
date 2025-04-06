710b1a8731ed748125f0bf8a016c47240a43b855
aleezel <alejandra.piedra02@gmail.com>
2025-04-06 07:32:56 -0600

    fix: correct method name from selectQueryAll to querySelectorAll for TextScrambleAnimation

--- src/main.js
+++ src/main.js
@@ -13,7 +13,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 4;
 
-TextScrambleAnimation(document.selectQueryAll("[terminal-text]")[0])
+TextScrambleAnimation(document.querySelectorAll("[terminal-text]")[0])
 
 const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('#effects'), antialias: true, alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
