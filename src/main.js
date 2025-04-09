e1ed67dff9f1f60f5292f1778fec418c1c3f71e4
Alejandra Piedra <89430034+aleezel@users.noreply.github.com>
2025-04-08 19:28:49 -0600

    Actualizar main.js
    SetTimeout added to scramble text

--- src/main.js
+++ src/main.js
@@ -12,8 +12,9 @@ import { TextScrambleAnimation } from "./textScrambleAnimation.js"
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 4;
-
-TextScrambleAnimation(document.querySelector('div[terminal-text] h2.home-subheading'))
+setTimeout(()=>{
+  TextScrambleAnimation(document.querySelector('div[terminal-text] h2.home-subheading'))
+}, 8000)
 
 const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('#effects'), antialias: true, alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
