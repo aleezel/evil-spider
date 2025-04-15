730e39f8474cedad14c1f9aa9c422a51f2dc6672
Alejandra Piedra <89430034+aleezel@users.noreply.github.com>
2025-04-15 07:12:51 -0600

    refresh priority en timelines, arreglar detalles

--- src/gsapTimelines.js
+++ src/gsapTimelines.js
@@ -26,20 +26,21 @@ export const gsapTimelines = () => {
             start: 'top top', // when the top of the trigger hits the top of the viewport
             end: 'bottom top', // end after scrolling 500px beyond the start
             scrub: 1, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
-            markers: true
+            markers: true,
+            refreshPriority: -1
         }
     });
 
     heroSecTl.addLabel('intro')
         .from('.spider-sticky', { backgroundImage: "linear-gradient(#AB074F, #8F1E73)", duration: 10 }, 0)
-        .fromTo('.copy-1', { autoAlpha: 0, y: "60vh" }, { autoAlpha: 1}, 0)
-        .to('.copy-1', { autoAlpha: 0, y: "-10vh" }, ">2")
-        .fromTo('.copy-2', { autoAlpha: 0, y: "60vh" }, { autoAlpha: 1}, ">-0.5" )
-        .to('.copy-2', { autoAlpha: 0, y: "-10vh" }, ">2")
-        .fromTo('.copy-3', { autoAlpha: 0, y: "60vh" }, { autoAlpha: 1}, ">-0.5" )
-        .to('.copy-2', { autoAlpha: 0, y: "-10vh" }, ">2")
+        .fromTo('.copy-1', { autoAlpha: 0, y: "60vh" }, { autoAlpha: 1}, duration: 8, 0)
+        .to('.copy-1', { delay: 10, autoAlpha: 0, y: "-10vh" }, ">10")
+        .fromTo('.copy-2', { autoAlpha: 0, y: "60vh" }, { autoAlpha: 1, duration: 8}, ">-0.5" )
+        .to('.copy-2', { delay: 10, autoAlpha: 0, y: "-10vh" }, ">10")
+        .fromTo('.copy-3', { autoAlpha: 0, y: "60vh" }, { autoAlpha: 1, duration: 8}, ">-0.5" )
+        .to('.copy-3', { delay: 10, autoAlpha: 0, y: "-10vh" }, ">10")
         .from('.hero-head_eyebrow', { autoAlpha: 0 })
-        .to('.copy-2', { autoAlpha: 1, y: "50vh" }, ">2")
+        .to('.hero-head_eyebrow', { autoAlpha: 1, y: "50vh" }, ">2")
 
         .from('.div-keyword', { autoAlpha: 0, stagger: 0.2 })
         
@@ -47,6 +48,7 @@ export const gsapTimelines = () => {
         .from('.spider-texture-wrap', { opacity: 0 })
         .from('.hero_spline', { opacity: 0 })
         .from('.main-text', { opacity: 0 })
+        .to({}, {duration: 5})
         
         .addLabel('end');
 
