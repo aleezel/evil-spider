281618e3b6173b24a34f067a02c9c6692b1011a9
Alejandra Piedra <alejandra.piedra02@gmail.com>
2026-07-12 21:10:10 -0600

    Fixed line break from hero Sscramble text

--- src/scrambleText-jp.js
+++ src/scrambleText-jp.js
@@ -6,22 +6,26 @@ import { ScrollTrigger } from 'gsap/ScrollTrigger'
 export const scrambleTextJp = () => {
     
     const scrambleTexts = document.querySelectorAll('[data-scrambletextauto]');
+    let tl = gsap.timeline();
 
-    scrambleTexts.forEach((text) => {
+    scrambleTexts.forEach((text, i) => {
         let ogText = text.innerText;
         let attribute = text.getAttribute('data-scrambletextauto');
         text.innerText = '';
 
-        gsap.to(text, {
+        tl.to(text, {
             scrambleText: {
                 text: ogText,
-                chars: '人類 社会のすべての構成員の固有の 尊厳と平等で譲ることのできない権利とを承認することは',
-                revealDelay: (attribute == 'intro') ? 3 : 0.5,
-                speed: 0.4,
-                oldClass:'dotgothic16-regular japanScramble'
+                chars: '人 類 !  社 会のすべ ての構成員の?固有の $ 尊厳と平等で譲ることのできない権利とを承認する¿ことは',
+                revealDelay: (attribute == 'intro') ? 3.5 : 0,
+                speed: 0.6,
+                oldClass:'dotgothic16-regular japanScramble',
+                delimiter: (i == 1) ? '' : '',
                 
             },
             duration: gsap.utils.clamp(3, ogText.length * 0.6, 7) ,
+            // duration: 10,
+            // delay: 6,
             ease: 'none',
         })
 
